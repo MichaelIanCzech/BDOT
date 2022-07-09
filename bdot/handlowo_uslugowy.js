1eec44228e1194c7c9f4a4b30678aaa10cd5f050
MichaelIanCzech <108399306+MichaelIanCzech@users.noreply.github.com>
2022-07-09 15:38:59 +0200

    Add files via upload

---
+++ bdot/handlowo_uslugowy.js
@@ -0,0 +1,8 @@
+var handlowo={
+"type": "FeatureCollection",
+"name": "handlowo-uslugowy",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "gml_id": "OT_KUHU_A.14", "lokalnyId": "13BE44CB-52D2-C16E-E053-CC2BA8C05E38", "przestrzen": "PL.PZGiK.994.BDOT10k", "wersjaId": "2021-02-12T00:00:00", "czyObiektB": 0, "x_kod": "KUHU02", "x_skrKarto": null, "x_katDoklG": "Prz", "x_zrodloDa": "Ort", "x_zrodlo_1": "Trn", "x_katIstni": null, "x_rodzajRe": "ZU", "x_aktualno": "2021-02-12", "x_aktual_1": "2018-07-11", "poczatekWe": "2021-02-12T00:00:00", "x_dataUtwo": "2013-01-21", "x_kodKarto": null, "x_kodKar_1": null, "x_kodKar_2": null, "x_kodKar_3": null, "x_kodKar_4": null, "x_kodKar_5": null, "x_kodKar_6": null, "nazwa": null, "rodzaj": "Trg" }, "geometry": { "type": "MultiPolygon", "coordinates": [ [ [ [ 17.648995740590784, 52.721603731113504 ], [ 17.649054901348194, 52.721443516541036 ], [ 17.649098998734129, 52.721348960664542 ], [ 17.649201543878235, 52.721145982930445 ], [ 17.64901722932564, 52.721109708739121 ], [ 17.64879969461353, 52.721094009885107 ], [ 17.648620499308084, 52.721098172961881 ], [ 17.648481305178372, 52.721121137900177 ], [ 17.647992883386202, 52.721406859876922 ], [ 17.64819256409379, 52.721487467548997 ], [ 17.648408039877971, 52.721545861821355 ], [ 17.648774749009622, 52.721647078274643 ], [ 17.648960628013491, 52.72170468458016 ], [ 17.648995740590784, 52.721603731113504 ] ] ] ] } }
+]
+}
